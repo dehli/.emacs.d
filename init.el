6baadf0de0a2ebc363b2257eab69f188b635e7f9
Christian Paul Dehli <dehli@gatech.edu>
2017-04-19 22:54:27 -0400

    Updated init.el

--- init.el
+++ init.el
@@ -1,19 +1,11 @@
-;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Packages
-;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Define package repositories
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
-
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
 
 ;; Load and activate emacs packages. Do this first so that the
 ;; packages are loaded before you start trying to modify them.
@@ -25,14 +17,6 @@
 ;; makes them available for download.
 (when (not package-archive-contents)
   (package-refresh-contents))
-
-;; Define he following variables to remove the compile-log warnings
-;; when defining ido-ubiquitous
-(defvar ido-cur-item nil)
-(defvar ido-default-item nil)
-(defvar ido-cur-list nil)
-(defvar predicate nil)
-(defvar inherit-input-method nil)
 
 ;; The packages you want installed. You can also install these
 ;; manually with M-x package-install
@@ -103,12 +87,7 @@
     ;; Snippet support
     yasnippet))
 
-;; On OS X, an Emacs instance started from the graphical user
-;; interface will have a different environment than a shell in a
-;; terminal window, because OS X does not run a shell during the
-;; login. Obviously this will lead to unexpected results when
-;; calling external utilities like make from Emacs.
-;; This library works around this problem by copying important
+;; This library is used to copying important
 ;; environment variables from the user's shell.
 ;; https://github.com/purcell/exec-path-from-shell
 (if (eq system-type 'darwin)
@@ -118,24 +97,9 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-
-;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
-;; to load them.
-;;
-;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
-;; then you can add the following code to this file:
-;;
-;; (Require 'yaml-mode)
-;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-;;
-;; Adding this code will make Emacs enter yaml mode whenever you open
-;; a .yml file
-(add-to-list 'load-path "~/.emacs.d/vendor")
-
-
-;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Customization
-;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Add a directory to our load path so that when you `load` things
 ;; below, Emacs knows where to look for the corresponding file.
@@ -170,6 +134,7 @@
 ;; Editor specific
 (load "setup-helm.el")
 (load "setup-auto-complete.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
