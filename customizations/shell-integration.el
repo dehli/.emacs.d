668eceb93dc4aa8c1fa093fee2a66f9bfc541491
Christian Paul Dehli <dehli@gatech.edu>
2018-06-27 17:58:03 -0400

    Source .env folder if it's at root of project

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -9,3 +9,14 @@
      "INSIGHTS_DB_PASSWORD"
      "XPA_AWS_SES_SECRET_ACCESS_KEY_ID"
      "XPA_USER_POOL_SECRET_KEY")))
+
+;; If there's an .env at the root of the Git project, then source it
+(let* ((project-dir (car (split-string (shell-command-to-string
+                                        "git rev-parse --show-toplevel"))))
+       (env-content (shell-command-to-string
+                     (concat "FILE=" project-dir "/.env && test -f $FILE && cat $FILE")))
+       (vars (split-string env-content)))
+
+  (dolist (elt vars)
+    (let ((value (split-string elt "=")))
+      (setenv (nth 0 value) (nth 1 value)))))
