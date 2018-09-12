10e49bc0ee8936a3d928c5c2b8b695f26ec98402
Christian Paul Dehli <dehli@gatech.edu>
2018-09-12 07:52:38 -0400

    Move shell stuff after .env sourcing

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -5,9 +5,6 @@
   (exec-path-from-shell-copy-envs
    '("PATH")))
 
-(setq shell-file-name "bash")
-(setq shell-command-switch "-ic")
-
 ;; If there's an .env at the root of the Git project, then source it
 (let* ((project-dir (car (split-string (shell-command-to-string
                                         "git rev-parse --show-toplevel"))))
@@ -18,3 +15,6 @@
   (dolist (elt vars)
     (let ((value (split-string elt "=")))
       (setenv (nth 0 value) (nth 1 value)))))
+
+(setq shell-file-name "bash")
+(setq shell-command-switch "-ic")
