97b3b3c61eec6d2d3d671ebf8e2c32d64e3a7e30
Christian Paul Dehli <cp@deh.li>
2022-02-08 23:58:56 -0500

    Bump straight versions

--- init.el
+++ init.el
@@ -8,6 +8,14 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 
 (load "bootstrap-straight.el")
+
+;; These lines can be removed once these issues are addressed
+;; https://github.com/emacs-helm/helm/issues/2481
+;; https://github.com/melpa/melpa/pull/7896
+(setq straight-recipe-overrides nil)
+(straight-override-recipe '(helm :files ("*.el" "emacs-helm.sh" (:exclude "helm-core.el" "helm-lib.el" "helm-source.el" "helm-multi-match.el"))))
+(straight-override-recipe '(helm-core :files ("helm-core.el" "helm-lib.el" "helm-source.el" "helm-multi-match.el")))
+
 (load "packages.el")
 
 (load "setup-ag.el")
