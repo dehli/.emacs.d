fb10462d216371a1cd9cda504e4652df860ea34d
Christian Paul Dehli <cp@deh.li>
2022-02-23 22:08:50 -0500

    add shortcut to display todo buffer

--- elisp/setup-org-mode.el
+++ elisp/setup-org-mode.el
@@ -8,24 +8,25 @@
 
 (use-package org)
 
-;; :init
-;; (setq org-roam-dailies-directory "daily/")
-
-;; (setq org-roam-dailies-capture-templates
-;;       '(("d" "default" entry
-;;          "* %?"
-;;          :target (file+head "%<%Y-%m-%d>.org"
-;;                             "#+title: %<%Y-%m-%d>\n"))))
-
 (use-package org-roam
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n t" . #'org-roam-display-todo))
 
   :commands (org-roam-db-autosync-enable)
 
   :config
   (org-roam-db-autosync-enable))
+
+(require 'org-roam-mode)
+(require 'org-roam-node)
+
+(defun org-roam-display-todo ()
+  "Display org-roam dedicated buffer for TODO."
+  (interactive)
+  (org-roam-buffer-display-dedicated
+   (org-roam-node-from-title-or-alias "TODO")))
 
 (provide 'setup-org-mode)
 ;;; setup-org-mode.el ends here
