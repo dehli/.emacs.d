a508a353da7ea4f32bec47e00709c5ceae221001
Christian Paul Dehli <cp@deh.li>
2023-04-29 11:18:57 -0400

    remove unneeded functions

--- elisp/kyber.el
+++ elisp/kyber.el
@@ -1,4 +1,4 @@
-;;; kyber.el -- Customizations related to various projects.
+;;; kyber.el -- Customizations related to projects.
 ;;
 ;;; Commentary:
 ;;
@@ -9,57 +9,6 @@
 
 (require 'cider)
 (require 'exec-path-from-shell)
-(require 'multi-vterm)
-(require 'vterm)
-
-(defun kyber-path (path)
-  "PATH is subfolder within KYBER_HOME."
-  (concat (exec-path-from-shell-copy-env "KYBER_HOME") path))
-
-(defun kyber-connect-node ()
-  "Start vterm process and connect to KYBER."
-  (interactive)
-  (let ((stack-name (ido-completing-read
-                     "Stack Name:" '("cp-dev" "kyber-development" "kyber-staging")))
-
-        (connect-parser (ido-completing-read
-                         "Pathom Viz:" '("false" "true"))))
-
-    (multi-vterm)
-
-    ;; Env vars are sent separately so that you can kill the node process
-    ;; and easily make changes to an individual variable if you wanted to.
-    (vterm-send-string "export AWS_PROFILE=arcx")
-    (vterm-send-return)
-    (vterm-send-string "set -a")
-    (vterm-send-return)
-    (vterm-send-string (concat "eval $(kyber.clj assume-role --stack-name " stack-name " eql-lambda)"))
-    (vterm-send-return)
-    (vterm-send-string (concat "eval $(kyber.clj load-lambda-env " stack-name "-EqlRoute)"))
-    (vterm-send-return)
-    (vterm-send-string "set +a")
-    (vterm-send-return)
-    (vterm-send-string (concat "export STACK_NAME=" stack-name))
-    (vterm-send-return)
-    (vterm-send-string (concat "export CONNECT_PARSER=" connect-parser))
-    (vterm-send-return)
-    (vterm-send-string "node $KYBER_HOME/server/dev/build/js/main.js")
-    (vterm-send-return)))
-
-(defun kyber-jack-in ()
-  "This function opens the Kyber project."
-  (interactive)
-  (setq cider-offer-to-open-cljs-app-in-browser nil)
-  (setq cider-preferred-build-tool 'shadow-cljs)
-  (setq cider-repl-display-help-banner nil)
-  (setq cider-shadow-default-options "server-dev")
-  (setq cider-shadow-cljs-parameters "server -A:test")
-  (cider-jack-in-cljs `(:cljs-repl-type shadow
-                        :project-dir ,(kyber-path "/server")))
-
-  ;; Give shadow enough time to compile. Ideally this would be triggered
-  ;; from an event, but I haven't learned enough about emacs/cider.
-  (run-at-time "30 sec" nil #'kyber-connect-node))
 
 (defun why-jack-in ()
   "This function opens the why project."
