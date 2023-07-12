e0e5671a42548230a6e0f462b77fa64f0c09a06d
Christian Paul Dehli <cp@deh.li>
2023-07-11 20:42:27 -0400

    add early-init.el

---
+++ early-init.el
@@ -0,0 +1,10 @@
+;;; early-init.el --- Early initialization file for Emacs
+
+;;; Commentary:
+;;  https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html
+
+;;; Code:
+
+(add-to-list 'default-frame-alist '(undecorated . t))
+
+;;; early-init.el ends here
